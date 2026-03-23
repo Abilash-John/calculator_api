0dcf9afc51bdab9c28b251f8b6992d6504aee1a8
Abilash-John <abilashjeyaraj@gmail.com>
2026-03-23 17:09:09 +0530

    Updates for Frontend/Migration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express from "express";
+import cors from "cors";
 import { AppDataSource } from "./config/data-source";
 import * as dotenv from "dotenv";
 
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
